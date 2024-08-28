8018dbc87e2d0b7e51bf0301b60c03bf7aff66f2
Jonathan Felicity <jonathanfelicity@mail.com>
2024-08-28 21:15:04 +0100

    added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // Set up global logging with the default logger
-    logger: ['log', 'error', 'warn', 'debug', 'verbose'], // Specify log levels if needed
-  });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('AUTOCHECK_ASSESMENT API  docs')
+    .setDescription('AUTOCHECK_ASSESMENT API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(process.env.PORT ?? 8081);
 }
 bootstrap();
