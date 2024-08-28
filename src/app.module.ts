e0e1ede98fcae48afdefa40b75b85c85a2ba7b1c
Jonathan Felicity <jonathanfelicity@mail.com>
2024-08-28 21:22:14 +0100

    add global validation and setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,22 +11,24 @@ import { LoanModule } from './loan/loans.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     VehicleModule,
     LoanModule,
     AuthModule,
-    ThrottlerModule.forRoot([{
-      name: 'short',
-      ttl: 1000,
-      limit: 3,
-    }, {
-      name: 'long',
-      ttl: 60000,
-      limit: 100,
-    }])],
+    ],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_GUARD,
     useClass: ThrottlerGuard,
   }],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): { message: String, status: String, timestamp: String } {
+    return {
+      message:
+        "🚀 Welcome aboard! You've just landed on our API. Fasten your seatbelt, and let's build something awesome!",
+      status: 'success',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
